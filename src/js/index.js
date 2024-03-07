e1c32b06c12c0241fb0f949dfbddca53c712c1b8
RoksanaKor <roksanakorban@gmail.com>
2024-03-07 22:46:47 +0100

    Create css files and js files

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,7 @@
 //TMDB Login: goit-js-group-03 hasło: goit-js-group-03
 
 API_KEY = '220ce7017a970ee5aafe37264f861af2';
+
+import {} from './full-info.js';
+import {} from './search.js';
+import {} from './trending.js';
